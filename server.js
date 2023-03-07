af876b9ed63da95592590318986fc55b395e927f
Paul Medeiros <paul.medeiros@graduway.com>
2023-03-07 08:04:27 -0800

    Add git workflow

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http");
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Status: OK");
+    res.end(JSON.stringify({ status: "OK", date: new Date().toISOString() }));
   })
-  .listen(process.env.PORT || 5000);
-console.log(`Listening on port: ${process.env.PORT || 5000}`);
+  .listen(process.env.PORT || 8080);
+console.log(`Listening on port: ${process.env.PORT || 8080}`);
